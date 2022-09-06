682a49ad3358a571c6989c807769edabc05ab3ff
David Marchante <davidmarchan@gmail.com>
2022-09-05 23:13:12 -0400

    Implement symmetric methods and mut variants

--- src/fourth.rs
+++ src/fourth.rs
@@ -2,7 +2,7 @@ use std::rc::Rc;
 
 // RefCell will enforce the borrowing rules at runtime instead of statically.
 // If you break the rules, the program will panic
-use std::cell::{RefCell, Ref};
+use std::cell::{Ref, RefCell, RefMut};
 
 pub struct List<T> {
     head: Link<T>,
@@ -51,6 +51,22 @@ impl<T> List<T> {
         }
     }
 
+    pub fn push_back(&mut self, elem: T) {
+        let new_tail = Node::new(elem);
+
+        match self.tail.take() {
+            Some(old_tail) => {
+                new_tail.borrow_mut().prev = Some(old_tail.clone());
+                old_tail.borrow_mut().next = Some(new_tail.clone());
+                self.tail = Some(new_tail);
+            }
+            None => {
+                self.head = Some(new_tail.clone());
+                self.tail = Some(new_tail);
+            }
+        }
+    }
+
     pub fn pop_front(&mut self) -> Option<T> {
         self.head.take().map(|old_head| {
             match old_head.borrow_mut().next.take() {
@@ -67,12 +83,49 @@ impl<T> List<T> {
         })
     }
 
+    pub fn pop_back(&mut self) -> Option<T> {
+        self.tail.take().map(|old_tail| {
+            match old_tail.borrow_mut().prev.take() {
+                Some(new_tail) => {
+                    new_tail.borrow_mut().next.take();
+                    self.tail = Some(new_tail);
+                }
+                None => {
+                    self.head.take();
+                }
+            }
+
+            Rc::try_unwrap(old_tail).ok().unwrap().into_inner().elem
+        })
+    }
+
     pub fn peek_front(&self) -> Option<Ref<T>> {
         self.head.as_ref().map(|node| {
             // It seems like in order to pass a borrowed reference
             // from a RefCell, you have to pass either a Ref or RefMut
             // Ref::map(Ref<T>, ..) -> Ref<U>
             Ref::map(node.borrow(), |node| &node.elem)
+        })
+    }
+
+    pub fn peek_front_mut(&self) -> Option<RefMut<T>> {
+        self.head.as_ref().map(|node| {
+            RefMut::map(node.borrow_mut(), |node| &mut node.elem)
+        })
+    }
+
+    pub fn peek_back(&self) -> Option<Ref<T>> {
+        self.tail.as_ref().map(|node| {
+            // It seems like in order to pass a borrowed reference
+            // from a RefCell, you have to pass either a Ref or RefMut
+            // Ref::map(Ref<T>, ..) -> Ref<U>
+            Ref::map(node.borrow(), |node| &node.elem)
+        })
+    }
+
+    pub fn peek_back_mut(&self) -> Option<RefMut<T>> {
+        self.tail.as_ref().map(|node| {
+            RefMut::map(node.borrow_mut(), |node| &mut node.elem)
         })
     }
 }
@@ -83,47 +136,75 @@ impl<T> Drop for List<T> {
     }
 }
 
-#[cfg(test)]
-mod test {
-    use super::List;
+#[test]
+fn basics() {
+    let mut list = List::new();
 
-    #[test]
-    fn basics() {
-        let mut list = List::new();
+    // Check empty list behaves right
+    assert_eq!(list.pop_front(), None);
 
-        // Check empty list behaves right
-        assert_eq!(list.pop_front(), None);
+    // Populate list
+    list.push_front(1);
+    list.push_front(2);
+    list.push_front(3);
 
-        // Populate list
-        list.push_front(1);
-        list.push_front(2);
-        list.push_front(3);
+    // Check normal removal
+    assert_eq!(list.pop_front(), Some(3));
+    assert_eq!(list.pop_front(), Some(2));
 
-        // Check normal removal
-        assert_eq!(list.pop_front(), Some(3));
-        assert_eq!(list.pop_front(), Some(2));
+    // Push some more just to make sure nothing's corrupted
+    list.push_front(4);
+    list.push_front(5);
 
-        // Push some more just to make sure nothing's corrupted
-        list.push_front(4);
-        list.push_front(5);
+    // Check normal removal
+    assert_eq!(list.pop_front(), Some(5));
+    assert_eq!(list.pop_front(), Some(4));
 
-        // Check normal removal
-        assert_eq!(list.pop_front(), Some(5));
-        assert_eq!(list.pop_front(), Some(4));
+    // Check exhaustion
+    assert_eq!(list.pop_front(), Some(1));
+    assert_eq!(list.pop_front(), None);
 
-        // Check exhaustion
-        assert_eq!(list.pop_front(), Some(1));
-        assert_eq!(list.pop_front(), None);
-    }
+    // ---- back -----
 
-    #[test]
-    fn peek() {
-        let mut list = List::new();
-        assert!(list.peek_front().is_none());
-        list.push_front(1);
-        list.push_front(2);
-        list.push_front(3);
+    // Check empty list behaves right
+    assert_eq!(list.pop_back(), None);
 
-        assert_eq!(&*list.peek_front().unwrap(), &3);
-    }
+    // Populate list
+    list.push_back(1);
+    list.push_back(2);
+    list.push_back(3);
+
+    // Check normal removal
+    assert_eq!(list.pop_back(), Some(3));
+    assert_eq!(list.pop_back(), Some(2));
+
+    // Push some more just to make sure nothing's corrupted
+    list.push_back(4);
+    list.push_back(5);
+
+    // Check normal removal
+    assert_eq!(list.pop_back(), Some(5));
+    assert_eq!(list.pop_back(), Some(4));
+
+    // Check exhaustion
+    assert_eq!(list.pop_back(), Some(1));
+    assert_eq!(list.pop_back(), None);
+}
+
+#[test]
+fn peek() {
+    let mut list = List::new();
+    assert!(list.peek_front().is_none());
+    assert!(list.peek_back().is_none());
+    assert!(list.peek_front_mut().is_none());
+    assert!(list.peek_back_mut().is_none());
+
+    list.push_front(1);
+    list.push_front(2);
+    list.push_front(3);
+
+    assert_eq!(&*list.peek_front().unwrap(), &3);
+    assert_eq!(&mut *list.peek_front_mut().unwrap(), &mut 3);
+    assert_eq!(&*list.peek_back().unwrap(), &1);
+    assert_eq!(&mut *list.peek_back_mut().unwrap(), &mut 1);
 }
