f516683003925d1bc4f0ea541ea00fa092cd5295
David Marchante <davidmarchan@gmail.com>
2022-09-06 00:41:08 -0400

    (Unsafe) Implement queue with raw pointer
    To test miri

---
+++ src/fifth.rs
@@ -0,0 +1,98 @@
+use std::ptr;
+
+pub struct List<T> {
+    head: Link<T>,
+    // If this was a Link (-> Box) we run into issues because
+    // Box owns the value, so we'd have to go back to Rc<RefCell> hell
+    // which makes .iter() and .iter_mut() a nightmare.
+    //
+    // An (unsafe) alternative would be to use a raw pointer
+    tail: *mut Node<T>,
+}
+
+type Link<T> = Option<Box<Node<T>>>;
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
+}
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        List {
+            head: None,
+            tail: ptr::null_mut(),
+        }
+    }
+
+    pub fn push(&mut self, elem: T) {
+        let mut new_tail = Box::new(Node { elem, next: None });
+
+        // Gotta grab the pointer before the value gets moved later
+        let raw_tail: *mut _ = &mut *new_tail;
+
+        if self.tail.is_null() {
+            self.head = Some(new_tail);
+        } else {
+            unsafe {
+                (*self.tail).next = Some(new_tail);
+            }
+        }
+
+        self.tail = raw_tail;
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        self.head.take().map(|head| {
+            self.head = head.next;
+
+            if self.head.is_none() {
+                self.tail = ptr::null_mut();
+            }
+
+            head.elem
+        })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::List;
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+
+        // Check empty list behaves right
+        assert_eq!(list.pop(), None);
+
+        // Populate list
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), Some(2));
+
+        // Push some more just to make sure nothing's corrupted
+        list.push(4);
+        list.push(5);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(4));
+
+        // Check exhaustion
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), None);
+
+        // Check the exhaustion case fixed the pointer right
+        list.push(6);
+        list.push(7);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(6));
+        assert_eq!(list.pop(), Some(7));
+        assert_eq!(list.pop(), None);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
-pub mod second;
-pub mod third;
+pub mod second; // a bad stack
+pub mod third;  // an ok stack
 pub mod fourth; // a bad but safe doubly-linked list
+pub mod fifth;  // an ok unsafe singly-linked queue
