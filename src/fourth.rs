84057a3660d5d42ec5e140965d4dbbf937cd608a
David Marchante <davidmarchan@gmail.com>
2022-09-05 23:33:37 -0400

    Implement IntoIter

--- src/fourth.rs
+++ src/fourth.rs
@@ -109,9 +109,9 @@ impl<T> List<T> {
     }
 
     pub fn peek_front_mut(&self) -> Option<RefMut<T>> {
-        self.head.as_ref().map(|node| {
-            RefMut::map(node.borrow_mut(), |node| &mut node.elem)
-        })
+        self.head
+            .as_ref()
+            .map(|node| RefMut::map(node.borrow_mut(), |node| &mut node.elem))
     }
 
     pub fn peek_back(&self) -> Option<Ref<T>> {
@@ -124,9 +124,9 @@ impl<T> List<T> {
     }
 
     pub fn peek_back_mut(&self) -> Option<RefMut<T>> {
-        self.tail.as_ref().map(|node| {
-            RefMut::map(node.borrow_mut(), |node| &mut node.elem)
-        })
+        self.tail
+            .as_ref()
+            .map(|node| RefMut::map(node.borrow_mut(), |node| &mut node.elem))
     }
 }
 
@@ -136,75 +136,119 @@ impl<T> Drop for List<T> {
     }
 }
 
-#[test]
-fn basics() {
-    let mut list = List::new();
+pub struct IntoIter<T>(List<T>);
 
-    // Check empty list behaves right
-    assert_eq!(list.pop_front(), None);
+impl<T> IntoIterator for List<T> {
+    type Item = T;
+    type IntoIter = IntoIter<T>;
 
-    // Populate list
-    list.push_front(1);
-    list.push_front(2);
-    list.push_front(3);
-
-    // Check normal removal
-    assert_eq!(list.pop_front(), Some(3));
-    assert_eq!(list.pop_front(), Some(2));
-
-    // Push some more just to make sure nothing's corrupted
-    list.push_front(4);
-    list.push_front(5);
-
-    // Check normal removal
-    assert_eq!(list.pop_front(), Some(5));
-    assert_eq!(list.pop_front(), Some(4));
-
-    // Check exhaustion
-    assert_eq!(list.pop_front(), Some(1));
-    assert_eq!(list.pop_front(), None);
-
-    // ---- back -----
-
-    // Check empty list behaves right
-    assert_eq!(list.pop_back(), None);
-
-    // Populate list
-    list.push_back(1);
-    list.push_back(2);
-    list.push_back(3);
-
-    // Check normal removal
-    assert_eq!(list.pop_back(), Some(3));
-    assert_eq!(list.pop_back(), Some(2));
-
-    // Push some more just to make sure nothing's corrupted
-    list.push_back(4);
-    list.push_back(5);
-
-    // Check normal removal
-    assert_eq!(list.pop_back(), Some(5));
-    assert_eq!(list.pop_back(), Some(4));
-
-    // Check exhaustion
-    assert_eq!(list.pop_back(), Some(1));
-    assert_eq!(list.pop_back(), None);
+    fn into_iter(self) -> Self::IntoIter {
+        IntoIter(self)
+    }
 }
 
-#[test]
-fn peek() {
-    let mut list = List::new();
-    assert!(list.peek_front().is_none());
-    assert!(list.peek_back().is_none());
-    assert!(list.peek_front_mut().is_none());
-    assert!(list.peek_back_mut().is_none());
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
 
-    list.push_front(1);
-    list.push_front(2);
-    list.push_front(3);
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop_front()
+    }
+}
 
-    assert_eq!(&*list.peek_front().unwrap(), &3);
-    assert_eq!(&mut *list.peek_front_mut().unwrap(), &mut 3);
-    assert_eq!(&*list.peek_back().unwrap(), &1);
-    assert_eq!(&mut *list.peek_back_mut().unwrap(), &mut 1);
+impl<T> DoubleEndedIterator for IntoIter<T> {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        self.0.pop_back()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+
+        // Check empty list behaves right
+        assert_eq!(list.pop_front(), None);
+
+        // Populate list
+        list.push_front(1);
+        list.push_front(2);
+        list.push_front(3);
+
+        // Check normal removal
+        assert_eq!(list.pop_front(), Some(3));
+        assert_eq!(list.pop_front(), Some(2));
+
+        // Push some more just to make sure nothing's corrupted
+        list.push_front(4);
+        list.push_front(5);
+
+        // Check normal removal
+        assert_eq!(list.pop_front(), Some(5));
+        assert_eq!(list.pop_front(), Some(4));
+
+        // Check exhaustion
+        assert_eq!(list.pop_front(), Some(1));
+        assert_eq!(list.pop_front(), None);
+
+        // ---- back -----
+
+        // Check empty list behaves right
+        assert_eq!(list.pop_back(), None);
+
+        // Populate list
+        list.push_back(1);
+        list.push_back(2);
+        list.push_back(3);
+
+        // Check normal removal
+        assert_eq!(list.pop_back(), Some(3));
+        assert_eq!(list.pop_back(), Some(2));
+
+        // Push some more just to make sure nothing's corrupted
+        list.push_back(4);
+        list.push_back(5);
+
+        // Check normal removal
+        assert_eq!(list.pop_back(), Some(5));
+        assert_eq!(list.pop_back(), Some(4));
+
+        // Check exhaustion
+        assert_eq!(list.pop_back(), Some(1));
+        assert_eq!(list.pop_back(), None);
+    }
+
+    #[test]
+    fn peek() {
+        let mut list = List::new();
+        assert!(list.peek_front().is_none());
+        assert!(list.peek_back().is_none());
+        assert!(list.peek_front_mut().is_none());
+        assert!(list.peek_back_mut().is_none());
+
+        list.push_front(1);
+        list.push_front(2);
+        list.push_front(3);
+
+        assert_eq!(&*list.peek_front().unwrap(), &3);
+        assert_eq!(&mut *list.peek_front_mut().unwrap(), &mut 3);
+        assert_eq!(&*list.peek_back().unwrap(), &1);
+        assert_eq!(&mut *list.peek_back_mut().unwrap(), &mut 1);
+    }
+
+    #[test]
+    fn into_iter() {
+        let mut list = List::new();
+        list.push_front(1);
+        list.push_front(2);
+        list.push_front(3);
+
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next_back(), Some(1));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next_back(), None);
+        assert_eq!(iter.next(), None);
+    }
 }
